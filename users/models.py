92d93008e5ff390b4d3db61ab983ca316de300a7
OolongTea620 <rlafls9596@gmail.com>
2022-01-25 18:40:13 +0900

    Edit: users, address

--- users/models.py
+++ users/models.py
@@ -1,26 +1,24 @@
 from django.db import models
 
 class User(models.Model) :
-    email = models.EmailField(max_length=300, unique=True)
-    password = models.CharField(max_length=250)
-    name = models.CharField(max_length=200)
+    
+    email           = models.EmailField(max_length=300, unique=True)
+    password        = models.CharField(max_length=250)
+    name            = models.CharField(max_length=200)
+    phone           = models.CharField(max_length=45, blank=True)
+    recive_letter   = models.BooleanField(default=False)
+    skin_type       = models.ForeignKey()
     
     class Meta :
-        db_table = 'users'
-
-class User_detail(models.Model) :
-    skintype = models.CharField(max_length=100, blank=True)
-    recive_letter = models.CharField(max_length=2, default='F')
-    
-    class Meta :
-        db_table = 'user_details'
+        db_table    = 'users'
 
 class Address(models.Model) :
-    nation_code = models.CharField(max_length=10, blank = True)
-    city = models.CharField(max_length=100, blank = True)
-    addr_detail = models.CharField(max_length=100, blank = True)
-    phone = models.CharField(max_length=50, blank = True)
-    post_number = models.CharField(max_length=50, blank = True)
+    
+    user_id         = models.ForeignKey(User, on_delete= models.CASCADE)
+    nation_code     = models.CharField(max_length=10, blank = True)
+    city            = models.CharField(max_length=100, blank = True)
+    addr_detail     = models.CharField(max_length=100, blank = True)
+    post_number     = models.CharField(max_length=50, blank = True)
 
     class Meta :
-        db_table = 'addresses'
+        db_table    = 'addresses'

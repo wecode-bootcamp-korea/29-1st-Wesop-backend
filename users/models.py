f6a26b20fbcfb312f6a44e08e25a360f9fa64b28
11292021 <xforwalkers@gmail.com>
2022-01-26 14:44:43 +0900

    Edit: User Model

--- users/models.py
+++ users/models.py
@@ -1,26 +1,25 @@
 from django.db import models
+from bases.models import Base
+from products.models import Skintype
 
-class User(models.Model) :
-    email = models.EmailField(max_length=300, unique=True)
-    password = models.CharField(max_length=250)
-    name = models.CharField(max_length=200)
-    
-    class Meta :
-        db_table = 'users'
-
-class User_detail(models.Model) :
-    skintype = models.CharField(max_length=100, blank=True)
-    recive_letter = models.CharField(max_length=2, default='F')
-    
-    class Meta :
-        db_table = 'user_details'
-
-class Address(models.Model) :
-    nation_code = models.CharField(max_length=10, blank = True)
-    city = models.CharField(max_length=100, blank = True)
-    addr_detail = models.CharField(max_length=100, blank = True)
-    phone = models.CharField(max_length=50, blank = True)
-    post_number = models.CharField(max_length=50, blank = True)
+class User(Base) :
+    email           = models.EmailField(max_length=255, unique=True)
+    password        = models.CharField(max_length=250)
+    name            = models.CharField(max_length=200)
+    phone           = models.CharField(max_length=45, blank=True)
+    receive_letter  = models.BooleanField(default=False)
+    skintype        = models.ForeignKey(Skintype, on_delete=models.CASCADE)
 
     class Meta :
-        db_table = 'addresses'
+        db_table    = 'users'
+
+
+class Address(Base) :
+    user            = models.ForeignKey(User, on_delete= models.CASCADE)
+    nation_code     = models.CharField(max_length=10, blank = True)
+    city            = models.CharField(max_length=100, blank = True)
+    address_detail  = models.CharField(max_length=100, blank = True)
+    post_number     = models.CharField(max_length=50, blank = True)
+
+    class Meta :
+        db_table    = 'addresses'
--- wesop/settings.py
+++ wesop/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from wesop.my_settings import SECRET_KEY, DATABASES
+from my_settings import SECRET_KEY, DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'bases',
     'users',
+    'orders',
+    'products',
 ]
 
 MIDDLEWARE = [

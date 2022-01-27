8a1b49e84cbcd2a05662bbaf135cd173cbc82b29
11292021 <xforwalkers@gmail.com>
2022-01-27 11:51:06 +0900

    Add: Product Modeling with CSV File

--- orders/models.py
+++ orders/models.py
@@ -1,35 +1,41 @@
+import uuid
+
 from django.db import models
-from products.models import Base, Product
+
+from products.models import Product
+from users.models import User
+from bases.models import Base
 
 class Cart(Base):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    user     = models.ForeignKey(User, on_delete=models.CASCADE)
+    product  = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
 
     class Meta:
         db_table = 'carts'
 
-class Order_Status(Base):
-    order_status = models.CharField(max_length=100)
+class OrderStatus(Base):
+    order_status = models.CharField(max_length=100, blank=True)
     
-    class Meta:
-        abstract = True
+    class meta:
+        db_table = 'order_status'
 
-class Order(Base, Order_Status):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    ordered_at = models.DateTimeField(auto_now=True)
-    ordered_code =  models.CharField(max_length=200)
+class Order(Base):
+    user         = models.ForeignKey(User, on_delete=models.CASCADE)
+    ordered_at   = models.DateTimeField(auto_now=True)
+    ordered_code = models.UUIDField(default=uuid.uuid4, editable=False)
+    order_status = models.ForeignKey(OrderStatus, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'orders'
 
-class Order_Item(Base, Order_Status):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
-    total_price = models.PositiveIntegerField()
-    tracking_number = models.CharField(max_length=300)
+class OrderItem(Base):
+    order           = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order_status    = models.ForeignKey(OrderStatus, on_delete=models.CASCADE)
+    product         = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity        = models.PositiveIntegerField(default=0)
+    total_price     = models.PositiveIntegerField(default=0)
+    tracking_number = models.CharField(max_length=300, blank=True)
 
     class Meta:
         db_table = 'order_items'
-    
--- products/models.py
+++ products/models.py
@@ -1,124 +1,59 @@
-from pydoc import describe
-from tkinter import CASCADE
 from django.db import models
-# from users import User
+from bases.models import Base
 
-class Base(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class Main_Category(Base):
+class MainCategory(Base):
     name = models.CharField(max_length=200)
     
     class Meta:
         db_table = 'main_categories'
 
-class Sub_Category(Base):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    main_category = models.ForeignKey(Main_Category, on_delete=models.CASCADE)
+class SubCategory(Base):
+    name          = models.CharField(max_length=200, blank=True)
+    description   = models.TextField()
+    main_category = models.ForeignKey(MainCategory, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'sub_catogries'
+        db_table = 'sub_categories'
 
 class Product(Base):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    name            = models.CharField(max_length=200)
+    description     = models.TextField()
     ingredients_etc = models.TextField()
-    sub_category = models.ForeignKey(Sub_Category)
+    sub_category    = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'products'
 
-class Product_Option(Base):
+class ProductOption(Base):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    size = models.CharField(max_length=30)
-    price = models.PositiveIntegerField()
+    size    = models.CharField(max_length=30)
+    price   = models.DecimalField(null=True, default=0, decimal_places=3, max_digits=9)
 
     class Meta:
         db_table = 'products_options'
 
-class Key_Ingredients(Base):
+class KeyIngredient(Base):
     name = models.CharField(max_length=100)
 
     class Meta:
         db_table = 'key_ingredients'
 
-class Product_Ingredient(Base):
-    ingredients = models.ForeignKey(Key_Ingredients, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+class ProductIngredient(Base):
+    ingredients = models.ForeignKey(KeyIngredient, on_delete=models.CASCADE)
+    product     = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'product_ingredients'
+        db_table = 'products_ingredients'
 
-class Skintypes(Base):
+class Skintype(Base):
     name = models.CharField(max_length=100)
 
     class Meta:
         db_table = 'skintypes'
 
-class Product_Skintype(Base):
+class ProductSkintype(Base):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    skin = models.ForeignKey(Skintypes, on_delete=models.CASCADE)
+    skin    = models.ForeignKey(Skintype, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'products_skintypes'
-
-# class User(Base):
-#     name = models.CharField(max_length=200)
-#     skintype = models.ForeignKey(Skintypes, on_delete=models.CASCADE)
-#     email = models.EmailField(max_length=300)
-#     password = models.CharField(max_length=250)
-#     phone = models.CharField(max_length=45)
-#     letter_received = models.BooleanField(default=False)
-
-#     class Meta:
-#         db_table = 'users'
-
-# class Address(Base):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     detail = models.CharField(max_length=200)
-#     city = models.CharField(max_length=100)
-#     post_number = models.CharField(max_length=50)
-#     nation_code = models.CharField(max_length=10)
-
-#     class Meta:
-#         db_table = 'addresses'    
-
-class Cart(Base):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
-
-    class Meta:
-        db_table = 'carts'
-
-# Order Status와 Order Item Status를 모두 처리
-class Order_Status(Base):
-    name = models.CharField(max_length=100)
-    
-    class Meta:
-        db_table = 'orders_statuses'
-
-class Order(Base):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    order_status = models.ForeignKey(Order_Status)
-    ordered_at = models.DateTimeField(auto_now=True)
-    ordered_code =  models.CharField(max_length=200)
-    
-    class Meta:
-        db_table = 'orders'
-
-class Order_Item(Base):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    order_status = models.ForeignKey(Order_Status, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
-    total_price = models.PositiveIntegerField()
-    tracking_number = models.CharField(max_length=300)
-
-    class Meta:
-        db_table = 'order_items'
-    
-
-

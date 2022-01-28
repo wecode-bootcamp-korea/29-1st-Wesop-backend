96099d2ad42feddf471d3bd54ab786a4b7827ff4
11292021 <xforwalkers@gmail.com>
2022-01-29 00:13:48 +0900

    Add: Product Views

--- products/models.py
+++ products/models.py
@@ -15,38 +15,39 @@ class SubCategory(Base):
     class Meta:
         db_table = 'sub_categories'
 
+class KeyIngredient(Base):
+    name       = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'key_ingredients'
+
 class Product(Base):
     name            = models.CharField(max_length=200)
     description     = models.TextField()
     ingredients_etc = models.TextField()
     sub_category    = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    ingredint       = models.ManyToManyField(KeyIngredient)
 
     class Meta:
         db_table = 'products'
-
+      
 class ProductOption(Base):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products_options')
     size    = models.CharField(max_length=30)
     price   = models.DecimalField(null=True, default=0, decimal_places=3, max_digits=9)
 
     class Meta:
         db_table = 'products_options'
 
-class KeyIngredient(Base):
-    name = models.CharField(max_length=100)
-
-    class Meta:
-        db_table = 'key_ingredients'
-
 class ProductIngredient(Base):
-    ingredients = models.ForeignKey(KeyIngredient, on_delete=models.CASCADE)
-    product     = models.ForeignKey(Product, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(KeyIngredient, on_delete=models.CASCADE, related_name='key_ingredients')
+    product     = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
 
     class Meta:
         db_table = 'products_ingredients'
 
 class Skintype(Base):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True)
 
     class Meta:
         db_table = 'skintypes'

f441424702d6fb60518209b403af16562b48d4f3
OolongTea620 <rlafls9596@gmail.com>
2022-02-07 14:02:39 +0900

    EDIT: product:model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from bases.models import Base
 
@@ -28,7 +29,8 @@ class ProductOption(Base):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     size    = models.CharField(max_length=30)
     price   = models.DecimalField(null=True, default=0, decimal_places=3, max_digits=9)
-
+    stock   = models.PositiveIntegerField(default=10, null=False)
+    
     class Meta:
         db_table = 'products_options'
 
